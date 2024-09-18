4384c24fb4337ce5f2da9df27965e79abd955ff5
boilmyplate <john.maranan@proton.me>
2024-09-18 13:38:43 +1200

    merge homepage

--- ctrl-c/src/app/homepage/Components/navbar/Navbar.jsx
+++ ctrl-c/src/app/homepage/Components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Link from "next/link"
-import Links from "./links/Links"
-import styles from './Navbar.module.css';
+import React from "react";
+import Link from "next/link";
+import Links from "./links/Links";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
-  return (
-    <div className ={styles.container}>
-      <div className={styles.logo}>Blank Web</div>
-      <div>
-        <Links/>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.logo}>Blank Web</div>
+			<div>
+				<Links />
+			</div>
+		</div>
+	);
+};
 
 export default Navbar;
