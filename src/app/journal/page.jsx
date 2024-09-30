f3ceb3da78fb227b999f4e13fc85740235f0f7c4
emilyylauu <Lauemily359@gmail.com>
2024-09-30 22:18:11 +1300

    linking to homepage

--- src/app/journal/page.jsx
+++ src/app/journal/page.jsx
@@ -37,7 +37,7 @@ const Journal = () => {
           <button className="button">Past Entries</button> {/* Button linking to past entries page */}
         </a>
 
-        <a href="/home-page">
+        <a href="/">
           <button className="button">Back to Homepage</button> {/* Button linking back to the homepage */}
           
         </a>
